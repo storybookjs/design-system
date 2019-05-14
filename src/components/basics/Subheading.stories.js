ecc791cc409b5bb7ce4179016e7d3ba4f48dcbc0
domyen <dom@hichroma.com>
2019-05-14 17:57:52 +0800

    Remove non DS components

--- src/components/basics/Subheading.stories.js
+++ src/components/basics/Subheading.stories.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Subheading } from './Subheading';
 
-storiesOf('basics/Subheading', module).add('subheading', () => <Subheading>Subheading</Subheading>);
+storiesOf('Design System|Subheading', module).add('subheading', () => (
+  <Subheading>Subheading</Subheading>
+));
