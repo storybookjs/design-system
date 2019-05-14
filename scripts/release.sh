9216b8d8e6e3cc44f469053cc6c494b1b56f743f
Michael Shilman <michael@lab80.co>
2019-05-14 11:37:16 +0800

    Add release script

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+export PATH=$(npm bin):$PATH
+
+VERSION=`auto version`
+
+## Support for label 'skip-release'
+if [ ! -z "$VERSION" ]; then
+  ## Update Changelog
+  auto changelog
+
+  ## Publish Package
+  npm version $VERSION -m "Bump version to: %s [skip ci]"
+  npm publish
+
+  ## Create GitHub Release
+  git push --follow-tags --set-upstream origin $branch
+  auto release
+fi
