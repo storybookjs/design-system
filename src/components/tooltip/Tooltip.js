cf83ba1cad0976316f3dfb63c2ede46bb7d39db9
domyen <dom@hichroma.com>
2019-05-16 20:22:43 +0800

    Improve tooltip shadows

--- src/components/tooltip/Tooltip.js
+++ src/components/tooltip/Tooltip.js
@@ -63,7 +63,7 @@ const TooltipWrapper = styled.div`
         rgba(248, 248, 248, 0.97) 0%,
         rgba(255, 255, 255, 0.97) 100%
       );
-      box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.05), 0 5px 15px 0 rgba(0, 0, 0, 0.1);
+      filter: drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.05)) drop-shadow(0 1px 3px rgba(0, 0, 0, 0.1));
       border-radius: 4px;
       font-size: ${typography.size.s1}px;
     `};
