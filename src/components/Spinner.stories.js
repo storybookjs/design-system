02010c225c83f94735f0e79a8d5adbfcaeff47a3
domyen <dom@hichroma.com>
2019-05-16 22:29:17 +0800

    Minor tweak

--- src/components/Spinner.stories.js
+++ src/components/Spinner.stories.js
@@ -4,22 +4,22 @@ import { Spinner } from './Spinner';
 
 storiesOf('Design System|Spinner', module)
   .add('default', () => (
-    <div style={{ background: 'white', padding: '3em', position: 'relative' }}>
+    <div style={{ background: 'white', padding: '1.5em', position: 'relative' }}>
       <Spinner />
     </div>
   ))
   .add('Inverse', () => (
-    <div style={{ background: 'black', padding: '3em', position: 'relative' }}>
+    <div style={{ background: 'black', padding: '1.5em', position: 'relative' }}>
       <Spinner inverse />
     </div>
   ))
   .add('inForm', () => (
-    <div style={{ background: '#eee', padding: '3em', position: 'relative' }}>
+    <div style={{ background: '#eee', padding: '1.5em', position: 'relative' }}>
       <Spinner inForm />
     </div>
   ))
   .add('inline', () => (
-    <div style={{ background: '#eee', padding: '3em', position: 'relative' }}>
+    <div style={{ background: '#eee', padding: '1.5em', position: 'relative' }}>
       <Spinner inline />
     </div>
   ));
