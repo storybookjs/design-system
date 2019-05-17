53b01e5ab5586f4eb65f6058a077150f6a89eb18
domyen <dom@hichroma.com>
2019-05-17 16:34:37 -0700

    Move ListItem to the /tooltip dir

--- src/components/tooltip/ListItem.js
+++ src/components/tooltip/ListItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-import { color, typography } from './shared/styles';
-import { inlineGlow } from './shared/animation';
+import { color, typography } from '../shared/styles';
+import { inlineGlow } from '../shared/animation';
 
 const Left = styled.span``;
 const Title = styled.span`
