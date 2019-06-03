4558f7fed8a91f5fc1d83d190517c54fec2e4c7c
Kyle Suss <susskyle@gmail.com>
2019-06-03 14:53:09 -0600

    wip: version

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,13 +5,13 @@ VERSION=`auto version`
 ## Support for label 'skip-release'
 if [ ! -z "$VERSION" ]; then
   ## Update Changelog
-  auto changelog
+  # auto changelog
 
   ## Publish Package
   npm version $VERSION -m "Bump version to: %s [skip ci]"
-  npm publish --access public
-
-  ## Create GitHub Release
-  git push --follow-tags --set-upstream origin $branch
-  auto release
+  # npm publish --access public
+  #
+  # ## Create GitHub Release
+  # git push --follow-tags --set-upstream origin $branch
+  # auto release
 fi
