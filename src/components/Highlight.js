16fd516e4bc782865cf7d22b2f0fbe83c6db8a41
Kyle Suss <susskyle@gmail.com>
2019-06-04 15:35:02 -0600

    Add Highlight component w/ bash, JS, TS, CSS, JSON support

---
+++ src/components/Highlight.js
@@ -0,0 +1,65 @@
+/* eslint-disable react/no-danger */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import Prism from 'prismjs';
+import loadLanguages from 'prismjs/components/index';
+import PropTypes from 'prop-types';
+
+import { color, typography } from './shared/styles';
+
+import 'prismjs/themes/prism.css';
+
+loadLanguages(['bash', 'typescript', 'json']);
+
+// Tweak Prism styling
+const HighlightBlock = styled.div`
+  *:not(pre) > code[class*='language-'],
+  pre[class*='language-'] {
+    background: ${color.lighter};
+    margin: 2em 0;
+  }
+
+  code[class*='language-'],
+  pre[class*='language-'] {
+    font-size: ${typography.size.s3}px;
+  }
+
+  code {
+    font-size: 17px;
+  }
+
+  .aside code {
+    font-size: 15px;
+  }
+`;
+
+class Highlight extends React.Component {
+  componentDidMount() {
+    this.highlightCode();
+  }
+
+  componentDidUpdate() {
+    this.highlightCode();
+  }
+
+  highlightCode() {
+    const domNode = ReactDOM.findDOMNode(this); // eslint-disable-line
+    Prism.highlightAllUnder(domNode);
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <HighlightBlock>
+        <div dangerouslySetInnerHTML={{ __html: children }} />
+      </HighlightBlock>
+    );
+  }
+}
+
+Highlight.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Highlight;
---
+++ src/components/Highlight.stories.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Highlight from './Highlight';
+
+const bash = `
+<pre class="language-bash"><code class="language-bash"><span class="token comment"># Highlight bash:</span>
+npx install some-package-name
+<span class="token function">cd</span> some-package-name
+</code></pre>
+`;
+
+const javascript = `
+<pre class="language-javascript"><code class="language-javascript"><span class="token comment">// Highlight JavaScript:</span>
+import React from 'react'
+
+const MyComponent = () => (
+  &#x3C;div>My component renders all the things&#x3C;/div>
+)
+
+export default MyComponent
+</code></pre>
+`;
+
+const typescript = `
+<pre class="language-javascript"><code class="language-javascript"><span class="token comment">// Highlight Typescript:</span>
+import React from 'react'
+
+interface InterfaceMyComponentProps {
+  isCool: boolean;
+}
+
+const MyComponent: React.SFC&#x3C;InterfaceMyComponentProps> = ({ isCool }) => (
+  &#x3C;div>My component isCool: {isCool}&#x3C;/div>
+)
+
+export default MyComponent
+</code></pre>
+`;
+
+const css = `
+<pre class="language-css"><code class="language-css"><span class="token comment">/* Highlight CSS: */</span>
+.someClass {
+  position: relative;
+}
+</code></pre>
+`;
+
+const json = `
+<pre class="language-json"><code class="language-json">{
+  "name": "@yourorg/package",
+  "version": "0.0.1",
+  "description": "A sweet package",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/yourorg/package.git"
+  }
+}
+</code></pre>
+`;
+
+storiesOf('Design System|Highlight', module)
+  .addParameters({ component: Highlight })
+  .add('all languages', () => (
+    <div>
+      <Highlight>{bash}</Highlight>
+      <Highlight>{javascript}</Highlight>
+      <Highlight>{typescript}</Highlight>
+      <Highlight>{css}</Highlight>
+      <Highlight>{json}</Highlight>
+    </div>
+  ));
