1c76251210e4559613108c3b13c779035c0881a2
Orestis Ioannou <oorestisime@gmail.com>
2019-06-15 10:48:08 +0200

    Enable a11y addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { load, addDecorator, addParameters } from '@storybook/react';
 import { getPropDefs } from '@storybook/addon-docs/react';
 import { DocsPage } from '@storybook/addon-docs/blocks';
+import { withA11y } from '@storybook/addon-a11y';
 import 'storybook-chromatic';
 
 addParameters({
   docs: DocsPage,
 });
+
+addDecorator(withA11y);
 
 import { GlobalStyle } from '../src/components/shared/global';
 addDecorator(story => (
