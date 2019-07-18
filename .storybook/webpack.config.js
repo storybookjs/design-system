aae05a01ad0d4b12e0563acee0a0feddca01208c
Michael Shilman <michael@lab80.co>
2019-07-18 14:45:43 +0800

    Upgrade SB Docs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,12 +3,7 @@ const path = require('path');
 module.exports = async ({ config }) => {
   config.module.rules.push({
     test: /\.stories\.js$/,
-    loaders: [
-      {
-        loader: require.resolve('@storybook/addon-storysource/loader'),
-        options: { injectParameters: true },
-      },
-    ],
+    loaders: [require.resolve('@storybook/source-loader')],
     include: [path.resolve(__dirname, '../src')],
     enforce: 'pre',
   });
