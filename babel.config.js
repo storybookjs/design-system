12c95216b711906bc35cfa26db2edf49ff8916ee
Kyle Holmberg <kholmberg@acorns.com>
2019-08-07 14:40:08 -0700

    Remove unnecessary babel plugin

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = api => {
 
   return {
     presets: ['@babel/env', '@babel/react'],
-    plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+    plugins: ['@babel/plugin-proposal-object-rest-spread'],
     env: {
       test: {
         plugins: ['require-context-hook'],
