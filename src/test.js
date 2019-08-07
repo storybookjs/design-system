969b8d6cc09b67f235509e8d045a7f8a4806c847
Kyle Holmberg <kholmberg@acorns.com>
2019-08-07 14:58:33 -0700

    Remove superfluous stuff

--- src/test.js
+++
@@ -1,8 +0,0 @@
-import { Radio } from '.';
-
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    const ExampleComponent = Radio;
-    expect(ExampleComponent).toBeTruthy();
-  });
-});
