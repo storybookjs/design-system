2f3b1d59e79c13be7d9759a7dc5df3eb7b6d853d
Michael Shilman <michael@lab80.co>
2019-08-12 17:15:04 +0800

    Add intro 'docs-only' story WIP

--- .storybook/config.js
+++ .storybook/config.js
@@ -14,4 +14,10 @@ addDecorator(story => (
 ));
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module);
+configure(
+  [
+    require.context('../src', true, /\.stories\.mdx$/),
+    require.context('../src', true, /\.stories\.js$/),
+  ],
+  module
+);
