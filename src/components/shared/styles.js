4c45927ef4544ebe596f448dab5147088aa52ac6
domyen <dom@hichroma.com>
2020-04-18 10:38:56 -0400

    Change primary color from coral to purple

--- src/components/shared/styles.js
+++ src/components/shared/styles.js
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: '#FF4785', // coral
+  primary: 'purple', // coral
   secondary: '#1EA7FD', // ocean
   tertiary: '#DDDDDD',
 
