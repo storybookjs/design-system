6dff6d74e37edeec86f5790b905e4d7423dbbe6c
Norbert de Langen <ndelangen@me.com>
2020-05-12 11:55:41 +0200

    ready for TS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
   addons: [
+    '@storybook/preset-typescript',
     '@storybook/addon-essentials',
     '@storybook/addon-docs',
     '@storybook/addon-actions',
