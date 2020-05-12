8d088d140f868a961260d0b5c1df2b472d158347
Norbert de Langen <ndelangen@me.com>
2020-05-12 13:49:53 +0200

    CLEANUP

--- src/components/Avatar.stories.js
+++ src/components/Avatar.stories.js
@@ -12,20 +12,17 @@ export default {
 
 export const Basic = argsStory({});
 
-export const large = () => {
-  console.log(Avatar.__docgenInfo);
-  return (
-    <div>
-      <Avatar isLoading size="large" />
-      <Avatar size="large" username="Tom Coleman" />
-      <Avatar
-        size="large"
-        username="Tom Coleman"
-        src="https://avatars2.githubusercontent.com/u/132554"
-      />
-    </div>
-  );
-};
+export const large = () => (
+  <div>
+    <Avatar isLoading size="large" />
+    <Avatar size="large" username="Tom Coleman" />
+    <Avatar
+      size="large"
+      username="Tom Coleman"
+      src="https://avatars2.githubusercontent.com/u/132554"
+    />
+  </div>
+);
 
 export const medium = () => (
   <div>
