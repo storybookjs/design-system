fa9c376e992c8f091c2f3b06264ee63f16478089
Norbert de Langen <ndelangen@me.com>
2020-06-03 13:51:07 +0200

    UPGRADES && CLEANUP redundant addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-docs',
-    '@storybook/addon-actions',
     '@storybook/addon-storysource',
     '@storybook/addon-a11y',
   ],
