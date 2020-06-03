7f36cb409e68e9b892b132e34514f155c2b60c04
Norbert de Langen <ndelangen@me.com>
2020-06-03 15:47:25 +0200

    CLEANUP

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
   addons: [
-    '@storybook/preset-typescript',
     '@storybook/addon-essentials',
     '@storybook/addon-storysource',
     '@storybook/addon-a11y',
