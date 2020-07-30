f73a1d87a3203e891db82bfa534d4619e3f3c1b6
Kyle Suss <susskyle@gmail.com>
2020-07-30 15:41:40 -0600

    Add line height to MenuLink

--- src/components/table-of-contents/MenuLink.js
+++ src/components/table-of-contents/MenuLink.js
@@ -6,4 +6,5 @@ import { Link } from '../Link';
 export const MenuLink = styled(({ isActive, ...rest }) => <Link {...rest} />)`
   color: ${(props) => (props.isActive ? color.secondary : color.darkest)};
   font-weight: ${(props) => (props.isActive ? typography.weight.bold : typography.weight.regular)};
+  line-height: 24px;
 `;
