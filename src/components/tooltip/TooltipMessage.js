2dcee430fd0a413f86b25c399e5d04847ac70180
D3Portillo <d3portillo@gmail.com>
2020-10-09 12:09:36 -0600

    removes previus ToolTipMessage changes
    This commit includes the delition of previus commit
    modding TooltipMEssage, this also includes an story
    for theming up the component

--- src/components/tooltip/TooltipMessage.js
+++ src/components/tooltip/TooltipMessage.js
@@ -32,9 +32,9 @@ const MessageWrapper = styled.div`
   box-sizing: border-box;
 `;
 
-export function TooltipMessage({ title, desc, links, className, ...rest }) {
+export function TooltipMessage({ title, desc, links, ...rest }) {
   return (
-    <MessageWrapper className={className} {...rest}>
+    <MessageWrapper {...rest}>
       <Message>
         {title && <Title>{title}</Title>}
         {desc && <Desc>{desc}</Desc>}
@@ -60,12 +60,10 @@ TooltipMessage.propTypes = {
       title: PropTypes.string.isRequired,
     })
   ),
-  className: PropTypes.string,
 };
 
 TooltipMessage.defaultProps = {
   title: null,
   desc: null,
   links: null,
-  className: '',
 };
