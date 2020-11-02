1e1da9a876c9b7896cf233ce3ad8849247bf29b4
Jono Kolnik <jono@toyboxsystems.com>
2020-11-02 13:29:55 -0500

    demo config

---
+++ .storybook/manager.js
@@ -0,0 +1,15 @@
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'right',
+  sidebarAnimations: true,
+  enableShortcuts: true,
+  isToolshown: true,
+  theme: undefined,
+  selectedPanel: 'contrast-app',
+  initialActive: 'sidebar',
+  showRoots: false,
+});
