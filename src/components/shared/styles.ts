8baec0bb3cdbe0f97d37ad1551ed5e1bf7350786
Shawn <shawn@massivelines.com>
2020-11-05 22:41:10 -0600

    Update primary color

--- src/components/shared/styles.ts
+++ src/components/shared/styles.ts
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: '#FF4785', // coral
+  primary: '#00ffff', // coral
   secondary: '#1EA7FD', // ocean
   tertiary: '#DDDDDD',
 
