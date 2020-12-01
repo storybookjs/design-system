5e3c1830e365be0b958f7eb9c8e8370cacd35dd0
Jono Kolnik <jono@toyboxsystems.com>
2020-12-01 14:28:44 -0500

    demo v2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,15 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
+  stories: [
+    '../src/components/Demo.stories.js',
+    '../src/components/Button.stories.js',
+    '../src/components/PriceHeader.stories.js',
+    '../src/components/Divider.stories.js',
+    '../src/components/DatePicker.stories.js',
+  ],
   addons: [
+    'storybook-contrast',
     '@storybook/addon-essentials',
     '@storybook/addon-storysource',
     '@storybook/addon-a11y',
-    'storybook-contrast',
   ],
 };
