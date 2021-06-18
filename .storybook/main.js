45e54092e02215fdb62190fcfd8be7fb81b35cfc
Tom Coleman <tom@thesnail.org>
2021-06-18 20:41:30 +1000

    Got working with refactored skeleton

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,36 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.@(mdx|js|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
+  skeletonWebpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.m?[t|j]sx?$/,
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /\.mdx/,
+          use: [
+            {
+              loader: 'null-loader',
+            },
+          ],
+        },
+      ],
+    },
+  },
 };
