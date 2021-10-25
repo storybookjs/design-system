9d829a200e44b978a3e376331abda6bb863114b4
Amber Smith <amber@chromatic.com>
2021-10-25 12:38:28 -0400

    - added custom color support to icon - pulled in breadcrumb from frontpage repo

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -11,7 +11,7 @@ const Svg = styled.svg<Partial<Props>>`
 `;
 
 const Path = styled.path`
-  fill: currentColor;
+  fill: ${(props) => props.color ?? 'currentColor'};
 `;
 
 /**
@@ -21,15 +21,18 @@ const Path = styled.path`
  * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
  * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
  */
-export const Icon: FunctionComponent<Props> = ({ icon, block = false, ...props }: Props) => {
+export const Icon: FunctionComponent<Props> = ({ icon, block = false, color, ...props }: Props) => {
   return (
     <Svg viewBox="0 0 1024 1024" width="20px" height="20px" block={block} {...props}>
-      <Path d={icons[icon]} />
+      <Path d={icons[icon]} color={color} />
     </Svg>
   );
 };
 
 interface Props {
   icon: keyof typeof icons;
+  /** Pass this prop to add display: block to the Icon */
   block?: boolean;
+  /** Set a custom color for the Icon */
+  color?: string;
 }
