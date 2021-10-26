d695668842613b2b307d2a521cf6dbac2691b1b8
Amber Smith <amber@chromatic.com>
2021-10-26 14:56:28 -0400

    - give icon color prop a default value - add story for custom colored icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -11,7 +11,7 @@ const Svg = styled.svg<Partial<Props>>`
 `;
 
 const Path = styled.path`
-  fill: ${(props) => props.color ?? 'currentColor'};
+  fill: ${(props) => props.color};
 `;
 
 /**
@@ -21,7 +21,12 @@ const Path = styled.path`
  * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
  * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
  */
-export const Icon: FunctionComponent<Props> = ({ icon, block = false, color, ...props }: Props) => {
+export const Icon: FunctionComponent<Props> = ({
+  icon,
+  block = false,
+  color = 'currentColor',
+  ...props
+}: Props) => {
   return (
     <Svg viewBox="0 0 1024 1024" width="20px" height="20px" block={block} {...props}>
       <Path d={icons[icon]} color={color} />
