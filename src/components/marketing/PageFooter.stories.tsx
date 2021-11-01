2028d1dc8efc24b68f30b0f535c2445b3b4b8054
Amber Smith <amber@chromatic.com>
2021-11-01 16:09:38 -0400

    - add logos + images - update imports - begin copying over footer from frontpage

---
+++ src/components/marketing/PageFooter.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { PageFooter } from './PageFooter';
+
+export default {
+  title: 'Marketing/PageFooter',
+  component: PageFooter,
+};
+
+export const Default = () => <PageFooter />;
