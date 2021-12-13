ed9716b0ac251adb559a90cfb9005a69d8bc3f01
Yann Braga <yannbf@gmail.com>
2021-12-13 12:12:51 +0100

    add addon-interactions and test-runner

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y', '@storybook/addon-interactions'],
   // features: {
   //   previewCsfV3: true,
   // },
