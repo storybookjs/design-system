3b8d48bc11901dfb39364f65a22bba78ee3a4311
Tom Coleman <tom@chromatic.com>
2022-03-31 20:46:57 +1100

    Got lazy compilation working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,17 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
+  features: {
+    storyStoreV7: true,
+  },
+  core: {
+    builder: {
+      name: 'webpack5',
+      options: {
+        lazyCompilation: true,
+      },
+    },
+  },
+  // Not sure why the option above doesn't do this in this project
+  webpackFinal: (c) => ({ ...c, experiments: { lazyCompilation: { entries: false } } }),
 };
