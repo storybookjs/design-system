b16e1ca639ba52a19ec7b7e27d160a3cadb44e52
Tom Coleman <tom@chromatic.com>
2022-05-26 15:52:11 +1000

    Don't compile story files and remove emotion sourcemaps

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = (api) => {
   api.cache(true);
 
   return {
+    sourceMaps: false,
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
     plugins: [
       '@babel/plugin-proposal-object-rest-spread',
@@ -9,6 +10,7 @@ module.exports = (api) => {
       [
         '@emotion',
         {
+          sourceMap: false,
           importMap: {
             '@storybook/theming': {
               styled: { canonicalImport: ['@emotion/styled', 'default'] },
