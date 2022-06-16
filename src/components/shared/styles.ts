9d10f8fe56e502cdafe7b0fe17a076962f0ded55
winkerVSbecks <varunvachhar@gmail.com>
2022-06-16 12:30:42 -0400

    add typography styles to shared styles

--- src/components/shared/styles.ts
+++ src/components/shared/styles.ts
@@ -39,6 +39,8 @@ export const color = {
   dark: '#666666',
   darker: '#444444',
   darkest: '#333333',
+  tr10: 'rgba(0 0 0 0.1)',
+  tr5: 'rgba(0 0 0 0.05)',
 
   border: 'rgba(0,0,0,.1)',
 
@@ -88,6 +90,7 @@ export const typography = {
 
 export const breakpoint = 600;
 export const pageMargin = 5.55555;
+export const breakpoints = [320, 600, 900];
 
 export const pageMargins = css`
   padding: 0 ${spacing.padding.medium}px;
@@ -126,4 +129,144 @@ export const hoverEffect = css`
 
 export const zIndex = {
   tooltip: 2147483647,
+};
+
+// Typography styles
+export const text = {
+  storybookMediumBold: css({
+    fontSize: 13,
+    fontWeight: typography.weight.bold,
+    lineHeight: '18px',
+  }),
+  storybookMedium: css({
+    fontSize: 13,
+    fontWeight: typography.weight.regular,
+    lineHeight: '18px',
+  }),
+  largeBold: css({
+    fontSize: typography.size.s3,
+    fontWeight: typography.weight.bold,
+    lineHeight: '24px',
+  }),
+  small: css({
+    fontSize: typography.size.s1,
+    fontWeight: typography.weight.regular,
+    lineHeight: '18px',
+  }),
+  regularBold: css({
+    fontSize: typography.size.s2,
+    fontWeight: typography.weight.bold,
+    lineHeight: '20px',
+  }),
+  smallBold: css({
+    fontSize: typography.size.s1,
+    fontWeight: typography.weight.bold,
+    lineHeight: '18px',
+  }),
+  large: css({
+    fontSize: typography.size.s3,
+    fontWeight: typography.weight.regular,
+    lineHeight: '24px',
+  }),
+  regular: css({
+    fontSize: typography.size.s2,
+    fontWeight: typography.weight.regular,
+    lineHeight: '20px',
+  }),
+};
+
+export const subheading = {
+  regular: css({
+    fontSize: 13,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '18px',
+    letterSpacing: '0.38em',
+  }),
+  small: css({
+    fontSize: 11,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '15px',
+    letterSpacing: '0.38em',
+  }),
+};
+
+export const headers = {
+  hero1: css({
+    fontSize: 48,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '65px',
+  }),
+  hero2: css({
+    fontSize: typography.size.l2,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '40px',
+  }),
+  h1: css({
+    fontSize: typography.size.l1,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '40px',
+  }),
+  h2: css({
+    fontSize: typography.size.m3,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '38px',
+  }),
+  h3: css({
+    fontSize: typography.size.m2,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '33px',
+  }),
+  h4: css({
+    fontSize: typography.size.m1,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '27px',
+  }),
+  h5: css({
+    fontSize: typography.size.s3,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '22px',
+  }),
+  h6: css({
+    fontSize: typography.size.s2,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '20px',
+  }),
+};
+
+export const code = {
+  regular: css({
+    fontFamily: typography.type.code,
+    fontSize: typography.size.s2,
+    fontWeight: typography.weight.regular,
+    lineHeight: '17px',
+  }),
+
+  small: css({
+    fontFamily: typography.type.code,
+    fontSize: typography.size.s1,
+    fontWeight: typography.weight.regular,
+    lineHeight: '14px',
+  }),
+};
+
+export const marketing = {
+  hero1: headers.hero2,
+
+  hero2: {
+    fontSize: typography.size.l2,
+    fontWeight: typography.weight.extrabold,
+    lineHeight: '55px',
+  },
+
+  textLarge: {
+    fontSize: typography.size.s3,
+    fontWeight: typography.weight.regular,
+    lineHeight: '28px',
+  },
+
+  textLargeBold: {
+    fontSize: typography.size.s3,
+    fontWeight: typography.weight.bold,
+    lineHeight: '28px',
+  },
 };
