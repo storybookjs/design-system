fc15015709a2f43ce609337764aac4112647294e
domyen <dom@hichroma.com>
2023-01-25 13:44:58 -0500

    Install css addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y', 'display-element-css'],
 };
