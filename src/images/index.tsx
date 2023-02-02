01578359b2d93b50cfd45c003db20de70a4623de
domyen <dom@hichroma.com>
2023-02-01 22:07:28 -0500

    Remove more unused components and assets

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -2,6 +2,5 @@ export { default as Chrome } from './Chrome';
 export { default as Firefox } from './Firefox';
 export { default as Ie } from './Ie';
 export { default as Safari } from './Safari';
-export * as AddonIcons from './marketing/addons';
 export * as Logos from './logos';
 export * as ColoredIcons from './colored-icons';
