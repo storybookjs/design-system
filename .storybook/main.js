48b5aaab137e72dece522cf82f384c3ca7b94a4a
domyen <dom@hichroma.com>
2023-03-08 18:35:24 -0500

    Attempt to upgrade to 7.0

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
+  stories: [
+    //'../src/**/*.mdx', 
+  '../src/**/*.stories.js', 
+  //'../src/components/*.stories.js', 
+  //'../src/**/*.stories.tsx'
+  
+],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    //autodocs: true
+  }
 };
