12b6425dbc1bcf1f1455b6e56396d4185eb9717b
domyen <dom@hichroma.com>
2023-03-08 18:41:11 -0500

    Revert main.js stories to normal

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,11 @@
 module.exports = {
-  stories: [
-    //'../src/**/*.mdx', 
-  '../src/**/*.stories.js', 
-  //'../src/components/*.stories.js', 
-  //'../src/**/*.stories.tsx'
-  
-],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
   },
   docs: {
-    //autodocs: true
+    autodocs: true
   }
 };
