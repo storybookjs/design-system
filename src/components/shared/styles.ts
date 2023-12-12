5cfdf9f88dbea2f85729649af3b922b921672c77
Jono Kolnik <1164060+JonathanKolnik@users.noreply.github.com>
2023-12-12 13:35:59 -0500

    Update styles.ts

--- src/components/shared/styles.ts
+++ src/components/shared/styles.ts
@@ -65,7 +65,7 @@ export const spacing = {
 
 export const typography = {
   type: {
-    primary: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+    primary: 'var(--font-nunito-sans, 'Nunito Sans'), "Helvetica Neue", Helvetica, Arial, sans-serif',
     code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
   },
   weight: {
